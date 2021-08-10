1d82900cd821afb5dc339996fc195ba867acce03
Fatima Zohra <fzohra71@gmail.com>
2021-08-10 20:39:17 +0530

    Add files via upload

---
+++ tha20/src/Food_data.jsx
@@ -0,0 +1,44 @@
+const Food = [
+    {
+      id: 0,
+      name: "Pizza",
+      amount: "266",
+    },
+    {
+      id: 1,
+      name: "Burger",
+      amount: "300",
+    },
+    {
+      id: 2,
+      name: "Coke",
+      amount: "220",
+    },
+    {
+      id: 3,
+      name: "Chowmein",
+      amount: "146",
+    },
+    {
+      id: 4,
+      name: "Brownie",
+      amount: "60",
+    },
+    {
+      id: 5,
+      name: "Lassania",
+      amount: "263",
+    },
+    {
+      id: 6,
+      title: "Pani Puri",
+      calories: "20",
+  },
+  {
+    id: 7,
+    title: "Fried Rice",
+    calories: "99",
+},
+  ];
+  
+  export default Food;
